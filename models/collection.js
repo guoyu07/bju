a1c71c9ca686b1adb073a09b81058f802852bf71
viking zhang <viking_zhang@apple.com>
2015-11-19 16:38:12 +0800

    fix bug in collection route

--- models/collection.js
+++ models/collection.js
@@ -50,15 +50,23 @@ Collection.prototype.find = function(filter, callback) {
                   });
 }
 Collection.prototype.findOne = function(id, callback) {
-	callback = callback = function() {};
+	callback = callback || function() {};
 	this._Collection.findById(id)
 									.populate({
 										path: '_creator',
 										select: '_id name'
 									})
+									.populate({
+										path: 'fans',
+										select: '_id name'
+									})
+									.populate({
+										path: 'songs',
+										select: 'sid title artist _creator fans url pic'
+									})
 									.exec(function(err, data) {
 										if (!err) {
-											callback(data)
+											callback(data);
 										} else {
 											console.error(err);
 										}
